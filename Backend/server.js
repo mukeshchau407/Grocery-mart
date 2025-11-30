5635236e2e58f7274479c3480eae1a1ba64867d3
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-11-30 19:58:38 +0545

    Add cart routes to server for handling cart API

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/user.route.js";
 import sellerRouter from "./routes/seller.route.js";
 import connectCloudinary from "./configs/cloudinary.js";
 import productRouter from "./routes/product.route.js";
+import cartRouter from "./routes/cart.route.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -26,6 +27,7 @@ app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/user", userRouter);
 app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
 
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
