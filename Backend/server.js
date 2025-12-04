01d722a24960dbd76a896e2fdd3b0f07c49d101f
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-12-04 20:19:03 +0545

    feat(api): add order routes and integrate orderRouter into server configuration

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,7 @@ import connectCloudinary from "./configs/cloudinary.js";
 import productRouter from "./routes/product.route.js";
 import cartRouter from "./routes/cart.route.js";
 import addressRouter from "./routes/address.route.js";
+import orderRouter from "./routes/order.route.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -30,6 +31,7 @@ app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
 
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
