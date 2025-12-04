003ea9a174136ffd9c23d05859e73509b35bec82
Mukesh Kumar Chaudhary <chymukesh5@gmail.com>
2025-12-04 20:12:29 +0545

    feat: add seller auth check and integrate axios into app context

--- Frontend/src/context/AppContext.jsx
+++ Frontend/src/context/AppContext.jsx
@@ -2,6 +2,10 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const AppContext = createContext();
 
@@ -14,6 +18,21 @@ export const AppContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState({});
   const [searchQuery, setSearchQuery] = useState({});
+
+  //Fetch seller status
+  const fetchSeller = async () => {
+    try {
+      const { data } = await axios.get("api/seller/isAuth");
+      if (data.success) {
+        setIsSeller(true);
+      } else {
+        setIsSeller(false);
+      }
+      // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      setIsSeller(false);
+    }
+  };
 
   //Fetch All Products
   const fetchProducts = async () => {
@@ -75,6 +94,7 @@ export const AppContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
+    fetchSeller();
     fetchProducts();
   }, []);
 
@@ -97,6 +117,7 @@ export const AppContextProvider = ({ children }) => {
     setSearchQuery,
     getCartCount,
     getCartAmount,
+    axios,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
